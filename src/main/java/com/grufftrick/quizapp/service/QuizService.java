cff2666f7e360e66c16e712db8097da43cb87f26
GruffTrick <gruff.trick@gmail.com>
2024-04-02 21:52:38 +0100

    added submitting answers for a quiz

--- src/main/java/com/grufftrick/quizapp/service/QuizService.java
+++ src/main/java/com/grufftrick/quizapp/service/QuizService.java
@@ -5,6 +5,7 @@ import com.grufftrick.quizapp.dao.QuizDAO;
 import com.grufftrick.quizapp.model.Question;
 import com.grufftrick.quizapp.model.QuestionWrapper;
 import com.grufftrick.quizapp.model.Quiz;
+import com.grufftrick.quizapp.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,5 +44,19 @@ public class QuizService {
         }
 
         return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDAO.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int correctAnswers = 0;
+        int count = 0;
+        for (Response response : responses) {
+            if(response.getResponse().equals(questions.get(count).getCorrectAnswer())) {
+                correctAnswers++;
+            }
+            count++;
+        }
+        return new ResponseEntity<>(correctAnswers, HttpStatus.OK);
     }
 }
